85c3fe463d3b376097f99447987ff6c424cff09f
VickyGe22 <zhangzhige233@gmail.com>
2024-03-04 02:02:27 +1030

    Update page.tsx

--- task-list/src/app/page.tsx
+++ task-list/src/app/page.tsx
@@ -1,8 +1,66 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import Addtodo from "./components/AddTodo";
+import Todolist from "./components/todolist";
+import TodoFilter from "./components/TodoFilter";
+import { useState } from "react";
+
+import { Todo } from "./types";
+
 export default function Home() {
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const [filter, setFilter] = useState<string>("all");
+
+  const addtodo = (text:string) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    }
+    setTodos([...todos, newTodo]);
+  }
+
+  const deleteTodo = (id:number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  const toggleTodo = (id:number) => {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        }
+      }
+      return todo;
+    }))
+  }
+
+
+  const getFilterTodos = () => {
+    switch (filter) {
+      case "completed":
+        return todos.filter(todo => todo.completed);
+      case "active":
+        return todos.filter(todo => !todo.completed);
+      default:
+        return todos;
+    }
+
+  }
+  
+
   return (
-    <h1>TodoList</h1>
+    <div>
+      <h1>TodoList</h1>
+      <Addtodo addTodo={addtodo}></Addtodo>
+      <Todolist todos={todos} deleteTodo={deleteTodo} toggletodo={toggleTodo}></Todolist>
+      <TodoFilter setFilter={getFilterTodos}></TodoFilter>
+    </div>
+    
+
   );
 }
